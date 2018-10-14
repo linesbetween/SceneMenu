4deea4050432ae834b7d2ccc3851b7c019e758ba
linesbetween <14285266+linesbetween@users.noreply.github.com>
2018-10-13 20:26:49 -0400

    level2, loading image

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class LoadOnClick : MonoBehaviour {
 
+	public GameObject loadingImage;
+
 	public void LoadScene(int level)
 	{
+		loadingImage.SetActive (true);
 		Application.LoadLevel (level);
 	}
 }
